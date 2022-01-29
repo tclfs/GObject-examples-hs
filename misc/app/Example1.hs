dc5f71de9b17a04ee7470d8e1bda41dc5fde8c25
tclfs <ideami@outlook.com>
2022-01-29 23:08:49 +0800

    simplify code using withManagedPtr

--- misc/app/Example1.hs
+++ misc/app/Example1.hs
@@ -11,15 +11,15 @@ main = do
   instance1 <- new Object []
   instance2 <- new Object []
 
-  putStrLn $ "The address of instance1 is " <> show (managedForeignPtr . toManagedPtr $ instance1)
-  putStrLn $ "The address of instance2 is " <> show (managedForeignPtr . toManagedPtr $ instance2)
+  withManagedPtr instance1 (\obj->putStrLn $ "The address of instance1 is " <> show obj)
+  withManagedPtr instance2 (\obj->putStrLn $ "The address of instance2 is " <> show obj)
 
   type1 <- gtypeFromInstance instance1
   type2 <- gtypeFromInstance instance2
-
+  
   class1 <- typeClassPeek type1
   class2 <- typeClassPeek type2
   
   -- Why are the addresses of the class of instance1 and instance2 different? 
-  putStrLn $ "The address of the class of instance1 is" <> show (managedForeignPtr . toManagedPtr $ class1)
-  putStrLn $ "The address of the class of instance2 is" <> show (managedForeignPtr . toManagedPtr $ class2)
+  withManagedPtr class1 (\cls->putStrLn $ "The address of the class of instance1 is" <> show cls)
+  withManagedPtr class2 (\cls->putStrLn $ "The address of the class of instance2 is" <> show cls)
